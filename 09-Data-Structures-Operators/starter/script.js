373297cf25723191c896d115a5cba2787add5744
LEELA-SRI <leelasric@gmail.com>
2022-01-30 20:17:25 +0530

    my version

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
-// Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -45,47 +40,391 @@ const restaurant = {
   },
 };
 
-//let teams = ['team1', 'team2'];
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
+///////////////////////////////////////Working With Strings
+/*
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+
+//retrieving elt at an index
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+//length of string
+console.log(airline.length);
+console.log('B737'.length);
+
+//index of an elt
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+//-1 = not found
+
+//slicing
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat 😬');
+  else console.log('You got lucky 😎');
 };
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('jonas'));
+console.log(typeof new String('jonas'));
+
+console.log(typeof new String('jonas').slice(1));
+
+
+const airline = 'TAP Air Portugal';
+
+//changing cases
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'jOnAS'; // Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// Comparing emails
+const email = 'hello@jonas.io';
+const loginEmail = '  Hello@Jonas.Io \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+
+//for cutting spaces
+// const trimmedEmail = lowerEmail.trim();
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// replacing
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replace('boo', 'gate'));
+
+//replacing all instances
+console.log(announcement.replaceAll('door', 'gate'));
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320neo';
+//check for a subtring
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Airb'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW ARirbus family');
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+
+
+// Split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('jonas schmedtmann');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(64637836));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard('334859493847755774747'));
+
+// Repeat
+const message2 = 'Bad weather... All Departues Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written 
+in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM 
+(see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+ Some_Variable 
+ calculate_AGE
+ delayed_departure
+ 
+ SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+ underscoreCase      ✅
+ firstName           ✅✅
+ someVariable        ✅✅✅
+ calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words,
+like a_b
+HINT 3: Start without worrying about the ✅.
+Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose,
+so start watching the solution in case you're stuck. 
+Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+//let count=0
+let btn=document.querySelector('button')
+
+btn.addEventListener('click',function(){
+  let text=document.querySelector('textarea').value
+  let elts=text.split('\n');
+  for (let [count,content] of elts.entries()){
+    // let parts=[...content.split('_')]
+    // let toreturn=[]
+    // toreturn.push(parts[0].toLowerCase()+parts[1][0].toUpperCase()+parts[1].slice(1).toLowerCase());
+    // for (let i of toreturn){
+      //   count+=1  
+      //   console.log(i.trim()+`${'✅'.repeat(count)}`)
+      let [one,two]=content.toLowerCase().trim().split('_')
+      let final=one+two[0].toUpperCase()+two.slice(1)
+      console.log(`${final.padEnd(20)}${'✅'.repeat(count+1)}`);
+    }
+  })
+  
+  */
+ 
+ // Data needed for a later exercise
+ const flights =
+   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+  let elts=flights.split('+');
+  for (let i of elts){
+    i=i.split(';')
+    let symbol=i[0].indexOf('_Delayed') ? '':'🛑'
+
+    let op=`${symbol} ${i[0].replaceAll('_',' ')} from ${i[1].slice(0,3).toUpperCase()} to ${i[2].slice(0,3).toUpperCase()} (${i[3].replace(':','h')})`;
+    console.log(op.padStart(50));
+  }
+/*
+////////////////////////////////////set
+let newset= new Set([1,2,3,4,5,4])
+console.log(newset);
+
+console.log(new Set('leela'));
+
+//length of a set
+console.log(newset.size);
+
+//checking for an elt
+console.log(newset.has(8));
+
+//adding to a set
+console.log(newset.add('hola'));
+
+//deleteing an elt
+newset.delete(4)
+
+//deleting all the elts
+//newset.clear()
+console.log(newset);
+
+//looping
+for (let i of newset) console.log(i);
+
+//conversion from set to array
+let array=[...newset]
+console.log(array);
+*/
+
+/*
+/////////////////////////////////////////map
+let newmap=new Map()
+
+//adding elts
+newmap.set(1,2)
+newmap.set('how','are you')
+newmap.set('who','r u')
+newmap.set('my','name').set('is','nirvana').set(true,'human').set(false,'plutonian')
+console.log(newmap);
+
+///////////////////reading data
+console.log(newmap.get(1));
+console.log(newmap.get(newmap.get('who')== 'r u'));
+
+///checking for an elt
+console.log(newmap.has('is'));
+
+///deleting an elt using a key
+newmap.delete(2)
+console.log(newmap);
+
+//deleting all using .clear()
+//length using .size
+
+// Maps: Iteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+
+////converting back to arrays
+let newarr=[...question]
+console.log(newarr);
+console.log(question.entries());
+console.log(question.keys());
+console.log([...question.values()]);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time,
+ we have a map with a log of the events that happened during the game.
+  The values are the events themselves, and the keys are the minutes 
+  in which each event happened (a football game has 90 minutes plus 
+    some extra time).
+
+1. Create an array 'events' of the different game events that happened
+ (no duplicates)
+2. After the game has finished, is was found that the yellow card from 
+minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened,
+ on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, 
+marking whether it's in the first half or second half 
+(after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+let events=[...new Set(gameEvents.values())]
+console.log(events);
+
+gameEvents.delete(64)
+let time=[...gameEvents.keys()].pop()
+console.log(`an event happened every ${time/gameEvents.size} mins`);
+
+for(let [k,v] of gameEvents){
+  console.log(k<45 ? `[FIRST HALF] ${k}:${v}` :`[SECOND HALF] ${k}:${v}`);
+}
+*/
+
+/*
 
 /*
 //////////////////////////////////looping
@@ -137,38 +476,38 @@ along with the goal number (Example: "Goal 1: Lewandowski")
 2. Use a loop to calculate the average odd and log it to the console 
 (We already studied how to calculate averages, 
   you can go check if you don't remember)
-3. Print the 3 odds to the console, but in a nice formatted way,
- exaclty like this:
+  3. Print the 3 odds to the console, but in a nice formatted way,
+  exaclty like this:
       Odd of victory Bayern Munich: 1.33
       Odd of draw: 3.25
       Odd of victory Borrussia Dortmund: 6.5
-Get the team names directly from the game object,
- don't hardcode them (except for "draw"). 
- HINT: Note how the odds and the game objects
-  have the same property names 😉
-
-BONUS: Create an object called 'scorers' which contains
- the names of the players who scored as properties, and 
- the number of goals as the value. In this game, it will look like this:
+      Get the team names directly from the game object,
+      don't hardcode them (except for "draw"). 
+      HINT: Note how the odds and the game objects
+      have the same property names 😉
+      
+      BONUS: Create an object called 'scorers' which contains
+      the names of the players who scored as properties, and 
+      the number of goals as the value. In this game, it will look like this:
       {
         Gnarby: 1,
         Hummels: 1,
         Lewandowski: 2
       }
 
-GOOD LUCK 😀
-
-
-
-for (let [num,player] of game.scored.entries()) console.log(`goal ${Number(num )+1} :${player}`);
-let i=0
-let odd=Object.values(game.odds)
-let key=Object.keys(game.odds)
-for (let scores of odd) i=i+scores
-console.log(i/odd.length);
-//for (let score of odd) console.log(`odd of ${key==='x' ? 'draw' : 'victory'} ${game[key]} : ${score}`);
-for ( let [k,v] of Object.entries(game.odds) ) console.log(`odd of ${ k==='x' ? 'draw' :'victory'} ${game[k] || ''}:${v}`);
-
+      GOOD LUCK 😀
+      
+      
+      
+      for (let [num,player] of game.scored.entries()) console.log(`goal ${Number(num )+1} :${player}`);
+      let i=0
+      let odd=Object.values(game.odds)
+      let key=Object.keys(game.odds)
+      for (let scores of odd) i=i+scores
+      console.log(i/odd.length);
+      //for (let score of odd) console.log(`odd of ${key==='x' ? 'draw' : 'victory'} ${game[key]} : ${score}`);
+      for ( let [k,v] of Object.entries(game.odds) ) console.log(`odd of ${ k==='x' ? 'draw' :'victory'} ${game[k] || ''}:${v}`);
+      
 let scorers={}
 for (let p of game.scored) {
   scorers[p] ? scorers[p]++ : scorers[p]=1;
@@ -295,7 +634,7 @@ let add=function(...nums){//packing here
   let sum=0;
   for (let i=0;i<nums.length;i++) sum+=nums[i]
   console.log(sum);
-
+  
 }
 add(2,3)
 add(5,6,3,9)
@@ -406,6 +745,51 @@ console.log(open,close);
 
 let {fri:{open:o,close:c}}=timings
 //renaming the values
+*/
+
+/*
+
+//let teams = ['team1', 'team2'];
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
 */
 
 /////////////////////////////////////////////////////////////////////////////
